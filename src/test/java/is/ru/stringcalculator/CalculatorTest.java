273040a1e2a00c57e6c22f9768c49b74eeee764a
fanney22 <fanneyhrund@gmail.com>
2014-10-16 13:20:57 +0000

    very many numbers
    added 21 numbers together

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -38,5 +38,10 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+	
+	@Test
+	public void testALargeNumberOfNumbers(){
+		assertEquals(21, Calculator.add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1"));
+	}
 
 }
